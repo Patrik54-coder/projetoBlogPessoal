d69512d1aa0b4329d590ae2569ab6572a2bdbaa9
Patrik Augusto Liro <patrikliro@gmail.com>
2021-05-13 11:20:27 -0300

    Função adm

--- Blog_Pessoal/blog/src/app/service/tema.service.ts
+++ Blog_Pessoal/blog/src/app/service/tema.service.ts
@@ -21,16 +21,20 @@ export class TemaService {
     return this.http.get<Tema[]>('http://localhost:8080/tema', this.token)
   }
 
+  getByIdTema(id: number): Observable<Tema>{
+    return this.http.get<Tema>(`http://localhost:8080/tema/${id}`, this.token)
+  }
+
+  getByNomeTema(nome: string): Observable<Tema[]>{
+    return this.http.get<Tema[]>(`http://localhost:8080/tema/nome/${nome}`, this.token)
+  }
+
   postTema(tema: Tema): Observable<Tema> {
     return this.http.post<Tema>('http://localhost:8080/tema', tema, this.token)
   }
 
   putTema(tema: Tema): Observable<Tema>{
     return this.http.put<Tema>('http://localhost:8080/tema', tema, this.token)
-  }
-
-  getByIdTema(id: number): Observable<Tema>{
-    return this.http.get<Tema>(`http://localhost:8080/tema/${id}`, this.token)
   }
 
   deleteTema(id: number){
